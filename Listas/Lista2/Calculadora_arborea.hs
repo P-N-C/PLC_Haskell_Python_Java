459e1df9d4baa46f68da748453e1b0dedae95fa7
Pedro <pnc@cin.ufpe.br>
2021-03-25 08:25:59 -0300

    Calculadora arborea adicionada

---
+++ Listas/Lista2/Calculadora_arborea.hs
@@ -0,0 +1,17 @@
+data Ops = SUM | MUL | SUB
+           deriving (Read)
+
+data IntTree = Nilt Int |
+               Node Ops IntTree IntTree
+               deriving (Read)
+
+evalTree :: IntTree -> Int
+evalTree (Nilt val) = val
+evalTree (Node SUM t1 t2) = (evalTree t1) + (evalTree t2)
+evalTree (Node SUB t1 t2) = (evalTree t1) - (evalTree t2)
+evalTree (Node MUL t1 t2) = (evalTree t1) * (evalTree t2)
+
+main = do
+    s <- getLine
+    let result = evalTree (read s)
+    print result
