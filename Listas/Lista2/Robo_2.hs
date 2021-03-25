0fd449c504982a13b6ae25edf38287ce6d2b6051
Pedro <pnc@cin.ufpe.br>
2021-03-25 08:23:32 -0300

    Robo 2 adicionado

--- Listas/Lista2/Robo_2.hs
+++ Listas/Lista2/Robo_2.hs
@@ -1,36 +1,27 @@
-import Prelude hiding (Maybe (..))
+data Command = Forward Int | Backward Int | TurnLeft | TurnRight 
+               deriving (Eq, Show, Read)
 
-data Maybe a = Just a |
-               Nothing
-               deriving(Show)
+data Direction = North | South | West | East
+                 deriving (Read, Show)
 
-getNum :: String -> String -> (Int,String)
-getNum [] num = (read num::Int, [])
-getNum (x:xs) num
-  | x >= '0' && x <= '9' = getNum xs (num++[x])
-  | length num == 0 = (0, (x:xs))
-  | otherwise = (read num::Int, (x:xs))
+newD :: Direction -> Command -> Direction
+newD d (Forward _) = d
+newD d (Backward _) = d
+newD North TurnLeft =  West
+newD North TurnRight = East
+newD South TurnLeft = East
+newD South TurnRight = West
+newD West TurnLeft = South
+newD West TurnRight = North
+newD East TurnLeft = North
+newD East TurnRight = South
 
-getOp :: String -> String -> (String,String)
-getOp [] op = (op,[])
-getOp (x:xs) op
-  | not (x >= '0' && x <= '9') = getOp xs (op++[x])
-  | otherwise = (op, (x:xs))
-
-safeCalc :: String -> IO ()
-safeCalc a = print (solve a)
-
-solve :: String -> Maybe Int
-solve str
-  | op == "div" && n2 == 0 = Nothing
-  | op == "sum" = Just (n1+n2)
-  | op == "sub" = Just (n1-n2)poi
-  | op == "mul" = Just (n1*n2)
-  | otherwise =   Just (n1`div`n2)
-    where (n1,str2) = getNum str []
-          (op,str3) = getOp str2 []
-          (n2,_) = getNum str3 []
+faces :: Direction -> [Command] -> Direction
+faces d [] = d
+faces d (x:xs) = faces (newD d x) xs
 
 main = do
        a <- getLine
-       safeCalc a
+       b <- getLine
+       let result = faces (read a) (read b)
+       print result
