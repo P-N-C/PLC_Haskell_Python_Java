996e4eb3fdc630ceef4ceda6d6ae6eccdea5a0c8
Pedro <pnc@cin.ufpe.br>
2021-03-25 08:36:46 -0300

    Altura de arvore adicionado

---
+++ Listas/Lista2/Altura_de_arvore.hs
@@ -0,0 +1,12 @@
+data Tree t = Node t (Tree t) (Tree t) 
+              | Nilt
+              deriving (Read)
+
+alturaArvore :: Tree t -> Int
+alturaArvore Nilt = 0
+alturaArvore (Node t t1 t2) = 1 + (max (alturaArvore t1) (alturaArvore t2))
+
+main = do
+       a <- getLine
+       let result = alturaArvore (read a::Tree Int)
+       print result
